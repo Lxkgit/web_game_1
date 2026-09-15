9dff9078020e1ee29fdc979638a9b25f47723369
GSZero <52993439+Lxkgit@users.noreply.github.com>
2026-09-15 16:11:14 +0800

    加入 NPC 与好感度基础数据

---
+++ src/data/npcs.js
@@ -0,0 +1,6 @@
+export const npcs = [
+  { id: 'mira', name: '米拉', icon: '👩‍🌾', role: '杂货店老板', favorite: ['flower', 'tea'], intro: '喜欢研究各种植物，也会偶尔给你带来稀有种子。' },
+  { id: 'kai', name: '凯恩', icon: '🧑‍🎣', role: '钓鱼达人', favorite: ['fish_trout', 'fish_carp'], intro: '每天都在河边，他似乎知道这条河所有秘密。' },
+  { id: 'luna', name: '露娜', icon: '👩‍🔬', role: '收藏家', favorite: ['crystal', 'gem', 'mushroom'], intro: '对未知的东西充满好奇，会奖励完成图鉴的玩家。' },
+  { id: 'old_bob', name: '老鲍勃', icon: '👴', role: '农场邻居', favorite: ['potato', 'tomato'], intro: '种了一辈子的地，偶尔会传授一些农业经验。' }
+]
