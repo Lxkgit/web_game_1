88d095027331a20fb301515a24f054878af074b9
GSZero <52993439+Lxkgit@users.noreply.github.com>
2026-09-15 16:11:00 +0800

    增加钓鱼种类与稀有度数据

---
+++ src/data/fish.js
@@ -0,0 +1,6 @@
+export const fish = [
+  { id: 'fish_roach', name: '溪鱼', icon: '🐟', rarity: 'common', chance: 55, sellPrice: 40 },
+  { id: 'fish_carp', name: '鲤鱼', icon: '🐠', rarity: 'uncommon', chance: 28, sellPrice: 75 },
+  { id: 'fish_trout', name: '虹鳟', icon: '🐡', rarity: 'rare', chance: 12, sellPrice: 120 },
+  { id: 'golden_fish', name: '金色锦鲤', icon: '✨', rarity: 'legendary', chance: 5, sellPrice: 500 }
+]
