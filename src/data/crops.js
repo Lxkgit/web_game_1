b9bab442302088ed32c98299f21a71e21bee37e2
GSZero <52993439+Lxkgit@users.noreply.github.com>
2026-09-15 16:10:54 +0800

    增加多种作物与成长数据

---
+++ src/data/crops.js
@@ -0,0 +1,6 @@
+export const crops = [
+  { id: 'potato', name: '土豆', icon: '🥔', seed: 'seed_potato', days: 2, sellPrice: 35, energy: 10, season: ['spring', 'autumn'] },
+  { id: 'carrot', name: '胡萝卜', icon: '🥕', seed: 'seed_carrot', days: 3, sellPrice: 45, energy: 12, season: ['spring', 'autumn'] },
+  { id: 'tomato', name: '番茄', icon: '🍅', seed: 'seed_tomato', days: 4, sellPrice: 55, energy: 14, season: ['spring', 'summer'] },
+  { id: 'strawberry', name: '草莓', icon: '🍓', seed: 'seed_strawberry', days: 5, sellPrice: 90, energy: 16, season: ['spring'] }
+]
